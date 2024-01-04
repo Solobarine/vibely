f534122b02ba579d8c5ed05fec4e97346841f8bf
solly <solobarine@gmail.com>
2024-01-04 19:10:57 +0100

    Share Country Api Key with Client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Inertia::share('country_api_key', config('app.country_api_key'));
     }
 
     /**
