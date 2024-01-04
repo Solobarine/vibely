024d9a35bb54061c1960d0f65d3ae6dc4718e9d9
solly <solobarine@gmail.com>
2024-01-04 18:24:56 +0100

    feat: Add Chat Component

---
+++ resources/js/Pages/Messenger/Components/Chat.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex items-start gap-2 py-3 px-2 bg-white">
+        <img src="user-female.png" alt="" class="w-10 shrink-0 grow-0 aspect-square rounded-full bg-primary">
+        <div class="grow">
+            <div class="grow flex items-center justify-between">
+                <p>Tanya Summers</p>
+                <small>Thursday</small>
+            </div>
+            <small>Let's do this</small>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* code... */
+</style>
