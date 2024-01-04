88be9fc7a70b6013405e2f579b15cc988ae81f3e
solly <solobarine@gmail.com>
2024-01-04 18:24:33 +0100

    feat: Add Friend Component

---
+++ resources/js/Pages/Friends/Components/Friend.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps(["auth", "friend"]);
+const friend =
+    props.auth.user.id === props.friend.user.id
+        ? props.friend.friend
+        : props.friend.user;
+console.log(props);
+</script>
+
+<template>
+    <div
+        class="relative max-w-[30em] sm:max-w-[20em] rounded-lg shadow-md bg-white w-full"
+    >
+        <div class="bg-stone-400 py-14 rounded-t-lg"></div>
+        <img
+            :src="friend.profile_photo_url"
+            alt="Friend"
+            class="w-[4em] absolute top-20 left-2 aspect-square rounded-full bg-primary"
+        />
+        <div class="pt-8 px-2 pb-2">
+            <Link
+                :href="route('user.profile', { username: friend.username })"
+                class="block"
+                >{{ friend.first_name }} {{ friend.last_name }}</Link
+            >
+            <small class="font-bold mt-2">@{{ friend.username }}</small>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Messenger/Components/Friend.vue
@@ -0,0 +1,24 @@
+<script setup>
+const { details } = defineProps(["details"]);
+console.log(details);
+</script>
+
+<template>
+    <div
+        @click="
+            $parent.$emit('showSplash', { status: false, fname: details.fname })
+        "
+        class="flex p-2 py-3 items-center gap-3 bg-white"
+    >
+        <img
+            src="user-male.png"
+            alt=""
+            class="w-10 shrink-0 grow-0 rounded-full bg-primary"
+        />
+        <p class="text-lg">{{ details.fname }} Sharp</p>
+    </div>
+</template>
+
+<style scoped>
+/* code... */
+</style>
