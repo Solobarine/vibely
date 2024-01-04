d35c10ba236f91d63a1688cd600b1d3ba4cc7822
solly <solobarine@gmail.com>
2024-01-04 16:51:23 +0100

    feat: Add config for local filesystem driver.

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,12 +34,22 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
             'throw' => false,
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -71,6 +81,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('')
     ],
 
 ];
