fec4a4e61afabfdd4c25c65f93443d0eee307301
solly <solobarine@gmail.com>
2024-01-04 18:26:36 +0100

    feat: Add Friend Component

---
+++ resources/js/Pages/Messenger/Partials/Friends.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Friend from "../Components/Friend.vue";
+defineEmits(["showSplash"]);
+const details = [
+    {
+        fname: "Jerry",
+    },
+    {
+        fname: "Tom",
+    },
+    {
+        fname: "Luke",
+    },
+    {
+        fname: "Terry",
+    },
+    {
+        fname: "Howard",
+    },
+    {
+        fname: "Dest",
+    },
+    {
+        fname: "Jude",
+    },
+];
+</script>
+
+<template>
+    <section class="grid gap-1">
+        <Friend v-for="i in 7" :key="i" :details="details[i - 1]" />
+    </section>
+</template>
