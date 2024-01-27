ba64a782731896abef93b4b1b64bd655e79faa3d
solly <solobarine@gmail.com>
2024-01-27 20:18:14 +0100

    Changes made:
    - Define Emits
    - Pass props to Chat Component

--- resources/js/Pages/Messenger/Partials/Chats.vue
+++ resources/js/Pages/Messenger/Partials/Chats.vue
@@ -1,10 +1,35 @@
 <script setup>
-import Chat from '../Components/Chat.vue';
+import Chat from "../Components/Chat.vue";
+
+defineEmits(["showSplash"]);
+const details = [
+    {
+        name: "Jerry Sienfeld",
+    },
+    {
+        name: "Tom Hanks",
+    },
+    {
+        name: "Luke Faring",
+    },
+    {
+        name: "Terry Crews",
+    },
+    {
+        name: "Howard Stern",
+    },
+    {
+        name: "Dest Johnson",
+    },
+    {
+        name: "Jude Foster",
+    },
+];
 </script>
 
 <template>
     <section class="grid gap-1">
-        <Chat v-for="i in 7" :key="i" />
+        <Chat v-for="i in 7" :key="i" :details="details[i - 1]" />
     </section>
 </template>
 
