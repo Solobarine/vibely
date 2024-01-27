0158035b4550888c9c88c43a48d28998c8310e16
solly <solobarine@gmail.com>
2024-01-27 20:19:10 +0100

    Changes made:
    - Accept Props and Populate prop values
    - Update styles
    - Emit action

--- resources/js/Pages/Messenger/Components/Chat.vue
+++ resources/js/Pages/Messenger/Components/Chat.vue
@@ -1,13 +1,22 @@
 <script setup>
-
+const { details } = defineProps(["details"]);
 </script>
 
 <template>
-    <div class="flex items-start gap-2 py-3 px-2 bg-white">
-        <img src="user-female.png" alt="" class="w-10 shrink-0 grow-0 aspect-square rounded-full bg-primary">
+    <div
+        @click="
+            $parent.$emit('showSplash', { status: true, name: details.name })
+        "
+        class="flex items-start gap-2 py-3 px-2 bg-white"
+    >
+        <img
+            src="user-female.png"
+            alt=""
+            class="w-10 shrink-0 grow-0 aspect-square rounded-full bg-primary"
+        />
         <div class="grow">
             <div class="grow flex items-center justify-between">
-                <p>Tanya Summers</p>
+                <p>{{ details.name }}</p>
                 <small>Thursday</small>
             </div>
             <small>Let's do this</small>
